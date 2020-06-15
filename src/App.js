42d74d1b53f3b72f1b5f690b416d36d747efc340
JZQ <1259516567@qq.com>
2020-06-15 15:05:38 +0800

    add: 添加公用文件夹及样式库

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import store from './store/index';
 import { Provider } from 'react-redux';
+import { GlobalStyle } from "./style";
+
 import Header from './components/header/index';
 
 function App() {
   return (
       <Provider store={ store }>
+          <GlobalStyle />
           <Header />
           <div>123</div>
       </Provider>
